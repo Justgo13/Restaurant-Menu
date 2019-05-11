3c662d4d94053e7d73b0662125db4eb30c56dcfa
jgao2 <jgao2@192.168.2.20>
2019-05-11 16:06:57 -0400

    final restaurant menu

---
+++ src/restaurant/menu/RestaurantMenu.java
@@ -0,0 +1,12 @@
+
+package restaurant.menu;
+
+public class RestaurantMenu {
+
+    public static void main(String[] args) {
+            MenuOutline menu = new MenuOutline();
+            menu.setVisible(true);
+            menu.setSize(800,450);
+    }
+    
+}
